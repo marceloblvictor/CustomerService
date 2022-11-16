ac30cab17bd57df818e8e32ec80ecf68b8f7ac10
Marcelo Victor <mvictor@innovaj.com>
2022-11-15 23:44:13 -0300

    first commit

---
+++ Data/CustomerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CustomerService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerService.Data;
+
+public class CustomerDbContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Host=localhost;Database=CustomersDB;Username=postgres;Password=581Marce!");
+}
+
---
+++ Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace CustomerService.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }        
+        public string? Name { get; set; } 
+    }
+}
